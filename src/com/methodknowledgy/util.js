a39a2d1fc0e4d1a3e20b3fcb8b3c9467fb9b2651
Jarrod Carlson <jarrod.carlson@gmail.com>
2009-02-11 05:43:11 +0000

    util package updated with new class names

--- src/com/methodknowledgy/util.js
+++ src/com/methodknowledgy/util.js
@@ -1,4 +1,6 @@
 dojo.provide("com.methodknowledgy.util");
-dojo.require("com.methodknowledgy.util.Collection");
 dojo.require("com.methodknowledgy.util.Iterable");
+dojo.require("com.methodknowledgy.util.Collection");
 dojo.require("com.methodknowledgy.util.List");
+dojo.require("com.methodknowledgy.util.Map");
+dojo.require("com.methodknowledgy.util.Set");
